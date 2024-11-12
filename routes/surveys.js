859038984ac2bde05b4f5dcc62dc7699755eef11
JackHeOtech <jack.he@ontariotechu.net>
2024-11-12 16:13:48 -0500

    updated surveys.js

--- routes/surveys.js
+++ routes/surveys.js
@@ -1,42 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const Survey = require('../models/Survey');
 
-let surveys = [];
-
-router.get('/', (req, res) => {
-  res.render('surveys/list', { title: 'Survey List', surveys });
+router.get('/', async (req, res) => {
+  try {
+    const surveys = await Survey.find();
+    res.render('surveys/list', { title: 'Survey List', surveys });
+  } catch (error) {
+    res.status(500).send("Could not receive Survey. Error.");
+  }
 });
 
 router.get('/create', (req, res) => {
   res.render('surveys/create', { title: 'Create Survey' });
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
   const { title, description } = req.body;
-  const newSurvey = { id: Date.now(), title, description };
-  surveys.push(newSurvey);
-  res.redirect('/surveys');
-});
-
-router.get('/edit/:id', (req, res) => {
-  const survey = surveys.find(s => s.id === parseInt(req.params.id));
-  res.render('surveys/edit', { title: 'Edit Survey', survey });
-});
-
-router.post('/edit/:id', (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
-  const survey = surveys.find(s => s.id === parseInt(id));
-  if (survey) {
-    survey.title = title;
-    survey.description = description;
+  try {
+    const newSurvey = new Survey({ title, description });
+    await newSurvey.save();
+    res.redirect('/surveys');
+  } catch (error) {
+    res.status(500).send("Survey could not be created. Error.");
   }
-  res.redirect('/surveys');
 });
 
-router.post('/delete/:id', (req, res) => {
-  surveys = surveys.filter(s => s.id !== parseInt(req.params.id));
-  res.redirect('/surveys');
+router.get('/edit/:id', async (req, res) => {
+  try {
+    const survey = await Survey.findById(req.params.id);
+    res.render('surveys/edit', { title: 'Edit Survey', survey });
+  } catch (error) {
+    res.status(500).send("Survey could not be received. Error..");
+  }
+});
+
+router.post('/edit/:id', async (req, res) => {
+  const { title, description } = req.body;
+  try {
+    await Survey.findByIdAndUpdate(req.params.id, { title, description });
+    res.redirect('/surveys');
+  } catch (error) {
+    res.status(500).send("Survey was not updated. Error.");
+  }
+});
+
+router.post('/delete/:id', async (req, res) => {
+  try {
+    await Survey.findByIdAndDelete(req.params.id);
+    res.redirect('/surveys');
+  } catch (error) {
+    res.status(500).send("Survey could not be deleted. Error.");
+  }
 });
 
 module.exports = router;
