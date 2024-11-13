a383a20473cefc3046c21d31a4230c9cf0ea6444
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-11-12 21:21:09 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const surveys = []; 
-  res.render('index', { title: 'Survey Site', surveys });
+  res.render('index', { title: 'Survey Site' });
 });
 
 module.exports = router;
