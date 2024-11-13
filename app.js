f8b1a54e398bfc25e74afcc06024b6719eaa4051
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-11-12 20:30:08 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var surveysRouter = require('./routes/surveys');
 
 var app = express();
 
-app.set('views', path.join(dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
