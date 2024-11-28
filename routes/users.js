3b97b733708a62a74e578472de41f05b6d366b52
JackHeOtech <jack.he@ontariotechu.net>
2024-11-28 14:15:02 -0500

    users.js route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/User');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/register', (req, res) => {
+  res.render('users/register', { title: 'Register' });
+});
+
+router.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    req.flash('success_msg', 'Registration complete. Please log in.');
+    res.redirect('/users/login');
+  } catch (error) {
+    req.flash('error_msg', 'Error registering user.');
+    res.redirect('/users/register');
+  }
+});
+
+router.get('/login', (req, res) => {
+  res.render('users/login', { title: 'Login' });
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/surveys',
+    failureRedirect: '/users/login',
+    failureFlash: true,
+  })
+);
+
+
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    req.flash('success_msg', 'You are logged out.');
+    res.redirect('/users/login');
+  });
 });
 
 module.exports = router;
