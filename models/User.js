5cf12466acfc694a2e7af4e38a7e0e2d7982ee0d
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-11-28 15:51:33 -0500

    test

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({ //Creates a user schema which allows the user to input a user and password
   username: { type: String, required: true, unique: true },
