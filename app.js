86ac54bd90d96a5efaa62246b5b9173f272644ce
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-11-29 14:42:51 -0500

    Updates

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var path = require('path');
 var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressLayouts = require('express-ejs-layouts'); 
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var surveysRouter = require('./routes/surveys');
 
 const session = require('express-session');
-const passport = require('./config/passport'); 
+const passport = require('./config/passport');
 const flash = require('connect-flash');
 
 var app = express();
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
   session({
-    secret: 'your_secret_key', 
+    secret: 'your_secret_key',
     resave: false,
     saveUninitialized: false,
   })
@@ -41,10 +41,10 @@ app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.title = 'Our Survey Site';
-  res.locals.user = req.user || null; 
-  res.locals.error_msg = req.flash('error_msg'); 
+  res.locals.user = req.user || null;
+  res.locals.error_msg = req.flash('error_msg');
   res.locals.success_msg = req.flash('success_msg');
-  res.locals.isAuthenticated = req.isAuthenticated(); 
+  res.locals.isAuthenticated = req.isAuthenticated();
   next();
 });
 
@@ -52,11 +52,11 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/surveys', surveysRouter);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
