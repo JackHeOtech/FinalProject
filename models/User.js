07f34ba3d2cbda3f3f52fe5d7c465746ce664961
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-12-05 16:38:25 -0500

    fix

--- models/User.js
+++ models/User.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({ //Creates a user schema which allows the user to input a user and password
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
+  googleId: { type: String },
+  facebookId: { type: String },
+  githubId: { type: String },
 });
 
-userSchema.pre('save', async function (next) { //Password check before savint to database and hashing to secure password
-  if (!this.isModified('password')) return next();
+// Password hashing before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // Only hash if password is modified
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-userSchema.methods.isValidPassword = async function (password) { //Ensures password matches one in schema
+// Check if the entered password is correct
+userSchema.methods.isValidPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
