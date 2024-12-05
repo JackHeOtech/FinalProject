d9bb838388ab82fd2874710872fda055aee89f3e
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-12-05 17:19:45 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-  secret: process.env.SECRET_KEY, 
-  database: process.env.MONGODB_URI
+  secret: process.env.SECRET_KEY,
+  database: process.env.MONGODB_URI,
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookAppId: process.env.FACEBOOK_APP_ID,
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
 };
