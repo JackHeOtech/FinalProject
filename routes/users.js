2e5e9c309ec2e30a37a81a7c5772fceb75390795
AhmedS003 <ahmed.shahid@ontariotechu.net>
2024-12-05 16:15:09 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -33,13 +33,49 @@ router.post(
   })
 );
 
-
 router.get('/logout', (req, res) => {
   req.logout((err) => {
     if (err) return next(err);
     req.flash('success_msg', 'You are logged out.');
     res.redirect('/users/login');
   });
+});
+
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/surveys',
+    failureRedirect: '/users/login',
+  })
+);
+
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get(
+  '/auth/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/surveys',
+    failureRedirect: '/users/login',
+  })
+);
+
+router.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get(
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: '/surveys',
+    failureRedirect: '/users/login',
+  })
+);
+
+router.get('/error', (req, res) => {
+  res.render('users/error', { title: 'Error' });
 });
 
 module.exports = router;
